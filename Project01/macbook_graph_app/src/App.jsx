3aa96585fafcae120b8389621b6c84c44a783952
philio1327 <philipanderegg@hotmail.com>
2026-03-28 01:41:55 -0400

    Created File Structure, Vite and Dependencies

---
+++ Project01/macbook_graph_app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Welcome to the MacBook Graph App!
+            </h1>
+            {/* Add your graph components and logic here */}
+        </div>
+    );
+}
+export default App;
