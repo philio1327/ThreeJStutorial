6853eba0a0b67cb17a59d9f3a3de75e6981bdcd8
philio1327 <philipanderegg@hotmail.com>
2026-04-01 22:27:35 -0400

    updated index.css for styling and utility classes

--- Project01/macbook_graph_app/src/App.jsx
+++ Project01/macbook_graph_app/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
+            <h1 className="text-3xl font-bold underline text-indigo-700">
                 Welcome to the MacBook Graph App!
             </h1>
             {/* Add your graph components and logic here */}
