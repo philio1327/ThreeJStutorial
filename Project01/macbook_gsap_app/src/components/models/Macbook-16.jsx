56aad1fbd23d4a21aab48e6848ec4dca4fa0e7c2
philio1327 <philipanderegg@hotmail.com>
2026-04-04 00:28:42 -0400

    Added SRGB Colorspace dependency to prevent Colour washout in models

--- Project01/macbook_gsap_app/src/components/models/Macbook-16.jsx
+++ Project01/macbook_gsap_app/src/components/models/Macbook-16.jsx
@@ -12,13 +12,16 @@ import React, {useEffect} from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 import useMacbookStore from '../../store';
 import { noChangeParts } from '../../constants';
-import { Color } from 'three';
+import { Color, SRGBColorSpace } from 'three';
 
 export default function MacbookModel16(props) {
   const { color } = useMacbookStore();
   const { nodes, materials, scene } = useGLTF('/models/macbook-16-transformed.glb');
 
   const texture = useTexture('/screen.png');
+  texture.colorSpace = SRGBColorSpace;
+  texture.needsUpdate = true;
+  
   useEffect(() => {
     scene.traverse((child) => {
       if (child.isMesh) {
