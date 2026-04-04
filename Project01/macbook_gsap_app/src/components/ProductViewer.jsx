528e599974d8badfae93b42ec6cffe3b50ae6bd8
philio1327 <philipanderegg@hotmail.com>
2026-04-03 20:34:26 -0400

    Installed threeJS and react-fiber packages, and updated Product Viewer for size and colour modifiers

--- Project01/macbook_gsap_app/src/components/ProductViewer.jsx
+++ Project01/macbook_gsap_app/src/components/ProductViewer.jsx
@@ -1,4 +1,5 @@
 import useMacbookStore from "../store";
+import clsx from "clsx";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
@@ -6,17 +7,33 @@ const ProductViewer = () => {
         <section id="product-viewer">
             
             <div className="controls">
-                <p className="info">MacBook Pro 16" in Space Black</p>
+                <p className="info">MacBook Pro {scale} in {color}</p>
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
-                        <div className="bg-neutral-300" />
-                        <div className="bg-neutral-900" />
+                        <div 
+                            onClick={() => setColor('#adb5bd')} 
+                            className={clsx("bg-neutral-300", color === '#adb5bd' && "active")} 
+                        />
+                        <div 
+                            onClick={() => setColor('#2e2c2e')} 
+                            className={clsx("bg-neutral-900", color === '#2e2c2e' && "active")} 
+                        />
                     </div>
 
                     <div className="size-control">
-                        <div><p>14"</p></div>
-                        <div><p>16"</p></div>
+                        <div 
+                            onClick={() => setScale(0.06)} 
+                            className={clsx(scale === 0.06 ? "bg-white text-black" : "bg-transparent text-white")} 
+                        >
+                            <p>14"</p>
+                        </div>
+                        <div 
+                            onClick={() => setScale(0.08)} 
+                            className={clsx(scale === 0.08 ? "bg-white text-black" : "bg-transparent text-white")} 
+                        >
+                            <p>16"</p>
+                        </div>
                     </div>
                 </div>
             </div>
