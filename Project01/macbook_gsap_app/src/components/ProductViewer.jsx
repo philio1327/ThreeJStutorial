07edbea16c2e4b32a67a3a062742ca5b05aaf8c1
philio1327 <philipanderegg@hotmail.com>
2026-04-03 20:21:00 -0400

    created Macbookstore inside index.jsx within the store folder and updated ProductViewer

--- Project01/macbook_gsap_app/src/components/ProductViewer.jsx
+++ Project01/macbook_gsap_app/src/components/ProductViewer.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import useMacbookStore from "../store";
 
 const ProductViewer = () => {
+    const { color, scale, setColor, setScale } = useMacbookStore();
     return (
-        <div>ProductViewer</div>
+        <section id="product-viewer">
+            
+            <div className="controls">
+                <p className="info">MacBook Pro 16" in Space Black</p>
+
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
+                        <div className="bg-neutral-300" />
+                        <div className="bg-neutral-900" />
+                    </div>
+
+                    <div className="size-control">
+                        <div><p>14"</p></div>
+                        <div><p>16"</p></div>
+                    </div>
+                </div>
+            </div>
+
+            <p className="text-white text-4xl">Render Canvas</p>
+        </section>
     )
 }
 export default ProductViewer;
