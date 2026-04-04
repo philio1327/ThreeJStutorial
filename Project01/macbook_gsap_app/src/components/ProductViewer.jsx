aa403865661cc28bcf3446d444c2d0adcc45767d
philio1327 <philipanderegg@hotmail.com>
2026-04-03 21:41:51 -0400

    Model reposition to center of canvas

--- Project01/macbook_gsap_app/src/components/ProductViewer.jsx
+++ Project01/macbook_gsap_app/src/components/ProductViewer.jsx
@@ -45,7 +45,7 @@ const ProductViewer = () => {
 
             <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
                 <StudioLights />
-                <MacbookModel14 scale={0.06} position={[-1, 0, 0]} />
+                <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
 
                 <OrbitControls enableZoom={false} />
             </Canvas>
