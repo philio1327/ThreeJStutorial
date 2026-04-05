38b4172c09bcc806835aab11b650ce48a4b42fbf
philio1327 <philipanderegg@hotmail.com>
2026-04-04 22:59:15 -0400

    Finished Performance Section with image ease-in-out animations on scroll

--- Project01/macbook_gsap_app/src/App.jsx
+++ Project01/macbook_gsap_app/src/App.jsx
@@ -3,8 +3,12 @@ import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import ProductViewer from './components/ProductViewer';
 import Showcase from './components/Showcase';
+import Performance from './components/Performance';
+import Features from './components/Features';
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Highlights from './components/Highlights';
+import Footer from './components/Footer';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +19,10 @@ const App = () => {
             <Hero />
             <ProductViewer />
             <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
         </main>
     );
 }
