7785408f6918b0fa70580b7b8f19302c1fe4b0b1
philio1327 <philipanderegg@hotmail.com>
2026-04-13 15:54:34 -0400

    Created footer section - links lead to homepage

--- Project01/macbook_gsap_app/src/components/Footer.jsx
+++ Project01/macbook_gsap_app/src/components/Footer.jsx
@@ -1,6 +1,26 @@
+import { footerLinks } from "../constants";
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className="info">
+                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 1-800-MY-APPLE</p>
+                <img src="/logo.svg" alt="Apple Logo" />
+            </div>
+
+            <hr />
+
+            <div className="links">
+                <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+                <ul>
+                    {footerLinks.map(({ label, link }) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
     )
 }
 export default Footer;
